49d0f44c2d506098adb3550ce3165f6783748ee7
Shogo Sakihama <goshalways@gmail.com>
2020-02-07 01:01:06 +0900

    セッション表示できなくなったから

--- views/header.inc.php
+++ views/header.inc.php
@@ -4,6 +4,6 @@
     <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
         <link rel="stylesheet" href="css/style.css">
         <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css' >
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
       </head>
