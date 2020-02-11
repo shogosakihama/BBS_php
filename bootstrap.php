44ecc1a5040daabeacf37f36b8b2dd0b679f9e18
Shogo Sakihama <goshalways@gmail.com>
2020-02-11 14:21:32 +0900

    2

--- bootstrap.php
+++ bootstrap.php
@@ -8,12 +8,14 @@ use Illuminate\Database\Eloquent\Model as Model;
 
 $capsule = new Capsule;
 
+$url = parse_url(getenv('DATABASE_URL'));
+
 $capsule->addConnection([
     'driver'    => 'pgsql',
-    'host'      => 'localhost',
-    'database'  => 'eloquent_test',
-    'username'  => 'shogo_sakihama',
-    'password'  => '',
+    'host'      => $url['host'],
+    'database'  => substr($url['path'], 1),
+    'username'  => $url['user'],
+    'password'  => $url['pass'],
     'charset'   => 'utf8',
     'collation' => 'utf8_unicode_ci',
     'prefix'    => '',
@@ -29,3 +31,25 @@ class Article extends Model {
 class User extends Model {
   
 }
+
+
+//
+// require_once 'vendor/autoload.php';
+
+// use Illuminate\Database\Capsule\Manager as Capsule;
+// use Illuminate\Database\Eloquent\Model as Model;
+
+
+// $capsule = new Capsule;
+
+// $capsule->addConnection([
+//     'driver'    => 'pgsql',
+//     'host'      => 'localhost',
+//     'database'  => 'eloquent_test',
+//     'username'  => 'shogo_sakihama',
+//     'password'  => '',
+//     'charset'   => 'utf8',
+//     'collation' => 'utf8_unicode_ci',
+//     'prefix'    => '',
+    
+// ]);
